abb9b319e3218241a555043ba333872fac47569d
Romainua <romanosadchiy458@gmail.com>
2023-05-10 18:49:00 +0200

    changed buttons positions

--- src/bot/keyboards/keyboard.js
+++ src/bot/keyboards/keyboard.js
@@ -2,9 +2,13 @@ function getKeyboard() {
    return {
       reply_markup: {
          keyboard: [
-            [{ text: 'Show Gas Price' }, { text: 'Set Gas' }, { text: 'Show My Validator' }],
-            [{ text: 'Add Validator' }, { text: 'Delete Validator' }, { text: 'Show Another Validator' }],
-            [{ text: 'Set Commission Rate' }, { text: 'Withdraw Rewards' }, { text: 'Show Rewards By Validator Name' }],
+            [
+               { text: 'Show Rewards By Validator Name' },
+               { text: 'Show Gas Price' },
+               { text: 'Show Another Validator' },
+            ],
+            [{ text: 'Add Validator' }, { text: 'Delete Validator' }, { text: 'Set Gas' }],
+            [{ text: 'Set Commission Rate' }, { text: 'Withdraw Rewards' }, { text: 'Show My Validator' }],
          ],
          resize_keyboard: true,
       },
