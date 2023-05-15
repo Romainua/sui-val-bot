5053ee7e73251098ed83414c2b5d31d154c5c28e
Romainua <romanosadchiy458@gmail.com>
2023-05-15 11:01:19 +0200

    init subscribe

--- src/bot/keyboards/keyboard.js
+++ src/bot/keyboards/keyboard.js
@@ -6,6 +6,7 @@ function getKeyboard() {
                { text: 'Show Rewards By Validator Name' },
                { text: 'Show Gas Price' },
                { text: 'Show Another Validator' },
+               { text: 'Delegate notifications' },
             ],
             [{ text: 'Add Validator' }, { text: 'Delete Validator' }, { text: 'Set Gas' }],
             [{ text: 'Set Commission Rate' }, { text: 'Withdraw Rewards' }, { text: 'Show My Validator' }],
@@ -15,4 +16,28 @@ function getKeyboard() {
    }
 }
 
-export default getKeyboard
+function subscribeKeyBoard() {
+   return {
+      inline_keyboard: [
+         [
+            { text: 'Delegation', callback_data: 'Delegation' },
+            { text: 'Undelegation', callback_data: 'Undelegation' },
+         ],
+         [{ text: 'Check current', callback_data: 'Check current' }],
+      ],
+   }
+}
+
+function backReply() {
+   return {
+      inline_keyboard: [[{ text: 'Back', callback_data: 'back_button' }]],
+   }
+}
+
+function unsubscribeCallBackButton(subscriptionsArray) {
+   return subscriptionsArray.map((obj) => {
+      return [{ text: `${obj.type} for ${obj.name}`, callback_data: obj.type }]
+   })
+}
+
+export { getKeyboard, subscribeKeyBoard, backReply, unsubscribeCallBackButton }
