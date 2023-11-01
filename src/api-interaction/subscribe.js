17f436db9250f0ca24bce4374f430ae5fccf46fd
Roman Osadchyi <romanosadchiy458@gmail.com>
2023-11-01 21:31:46 +0100

    update events request

--- src/api-interaction/subscribe.js
+++ src/api-interaction/subscribe.js
@@ -4,52 +4,50 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 async function initWsConnection() {
-   const ws = new WebSocket(process.env.WEBSOCKET_apiUrl)
-   return ws
+  const ws = new WebSocket(process.env.WEBSOCKET_apiUrl)
+  return ws
 }
 //connection for stake events
 async function createWebSocketConnection(validatorAddress, type) {
-   const ws = await initWsConnection()
+  const ws = await initWsConnection()
 
-   const requestData = {
-      jsonrpc: '2.0',
-      id: 1,
-      method: 'suix_subscribeEvent',
-      params: [
-         {
-            And: [
-               {
-                  MoveEventField: {
-                     path: '/validator_address',
-                     value: validatorAddress,
-                  },
-               },
-               {
-                  MoveEventType: `0x3::validator::${
-                     type === 'delegate' ? 'StakingRequestEvent' : 'UnstakingRequestEvent'
-                  }`,
-               },
-            ],
-         },
-      ],
-   }
+  const requestData = {
+    jsonrpc: '2.0',
+    id: 1,
+    method: 'suix_subscribeEvent',
+    params: [
+      {
+        And: [
+          {
+            MoveEventField: {
+              path: 'validator_address',
+              value: validatorAddress,
+            },
+          },
+          {
+            MoveEventType: `0x3::validator::${type === 'delegate' ? 'StakingRequestEvent' : 'UnstakingRequestEvent'}`,
+          },
+        ],
+      },
+    ],
+  }
 
-   ws.on('open', function open() {
-      console.log('WebSocket connection established')
+  ws.on('open', function open() {
+    console.log('WebSocket connection established')
 
-      ws.send(JSON.stringify(requestData)) //send requst
+    ws.send(JSON.stringify(requestData)) //send requst
 
-      setInterval(() => {
-         ws.ping()
-      }, 5000)
-   })
+    setInterval(() => {
+      ws.ping()
+    }, 5000)
+  })
 
-   //when we get error
-   ws.on('error', function error(err) {
-      console.error('WebSocket encountered error: ', err)
-   })
+  //when we get error
+  ws.on('error', function error(err) {
+    console.error('WebSocket encountered error: ', err)
+  })
 
-   return ws
+  return ws
 }
 
 export default createWebSocketConnection
