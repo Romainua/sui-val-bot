d8cffaf3973a4206d3618760a13490f825502ab9
Roman Osadchyi <romanosadchiy458@gmail.com>
2024-10-24 16:22:12 +0200

    update discord auth handlers

--- scripts/discord-auth-server.js
+++ scripts/discord-auth-server.js
@@ -20,7 +20,10 @@ const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN
 const DISCORD_BOT_TOKEN = process.env.DISCORD_BOT_TOKEN
 
 const DISCORD_API_USERS_URL = 'https://discord.com/api/v10/users/@me'
-const DISCORD_API_OAUTH2_URL = 'https://discord.com/api/oauth2/token'
+const DISCORD_API_OAUTH2_URL = 'https://discord.com/api/v10/oauth2/token'
+
+let accessToken
+let refreshToken
 
 app.get('/auth/discord/callback', async (req, res) => {
   const { code, state } = req.query
@@ -31,7 +34,73 @@ app.get('/auth/discord/callback', async (req, res) => {
   const telegramChatId = state
 
   try {
-    const tokenResponse = await axios.post(
+    const { access_token, refresh_token } = await getAccessToken(code)
+    accessToken = access_token
+    refreshToken = refresh_token
+
+    const user = await fetchDiscordUserData(accessToken)
+
+    const hasRequiredRole = await checkUserRole(user.id)
+
+    if (hasRequiredRole) {
+      await handleUpdateVerification(telegramChatId, hasRequiredRole)
+      const successMessage = `
+        ✅ **You are a verified member ${user.username}!** 🎉
+        
+        Welcome, validator! Your role has been verified, you now have access to exclusive announcements and updates.
+          `
+      await sendTelegramMessage(telegramChatId, successMessage, hasRequiredRole)
+      res.send('Success!')
+    } else {
+      const failureMessage = `❌ Hello ${user.username}, you do not have the required role.`
+      await sendTelegramMessage(telegramChatId, failureMessage, hasRequiredRole)
+      res.send('Failure! You do not have the required role.')
+    }
+  } catch (error) {
+    logger.error(
+      `Error during Discord authentication or role checking: ${
+        JSON.stringify(error.response?.data) || JSON.stringify(error.message)
+      }`,
+    )
+    console.error(error)
+    res.send('Authentication failed.')
+  }
+})
+
+// Default route for invalid paths
+app.get('/', (req, res) => {
+  res.send('Invalid route. Please use the correct callback URL.')
+})
+
+// Start the server
+app.listen(port, () => {
+  logger.info(`Discord auth server running on http://localhost:${port}`)
+})
+
+// Function to fetch user information
+async function fetchDiscordUserData(accessToken) {
+  try {
+    const response = await axios.get(DISCORD_API_USERS_URL, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    })
+
+    return response.data
+  } catch (error) {
+    if (error.response && error.response.status === 401) {
+      logger.error('Access token expired, refreshing token...')
+      const { access_token } = await refreshAccessToken()
+      return await fetchDiscordUserData(access_token)
+    }
+
+    throw new Error(`Error fetching user data: ${error.message}`)
+  }
+}
+// Function to get access token using the authorization code
+async function getAccessToken(code) {
+  try {
+    const response = await axios.post(
       DISCORD_API_OAUTH2_URL,
       new URLSearchParams({
         client_id: CLIENT_ID,
@@ -47,56 +116,61 @@ app.get('/auth/discord/callback', async (req, res) => {
       },
     )
 
-    const { access_token } = tokenResponse.data
+    return response.data
+  } catch (error) {
+    throw new Error(`Failed to get access token: ${error.message}`)
+  }
+}
 
-    const userResponse = await axios.get(DISCORD_API_USERS_URL, {
-      headers: {
-        Authorization: `Bearer ${access_token}`,
-      },
-    })
-
-    const user = userResponse.data
-
-    const guildMemberResponse = await axios.get(`https://discord.com/api/v10/guilds/${GUILD_ID}/members/${user.id}`, {
+// Function to check if the user has the required role
+async function checkUserRole(userId) {
+  try {
+    const response = await axios.get(`https://discord.com/api/v10/guilds/${GUILD_ID}/members/${userId}`, {
       headers: {
         Authorization: `Bot ${DISCORD_BOT_TOKEN}`,
       },
     })
 
-    const member = guildMemberResponse.data
-    const userRoles = member.roles
-
-    const hasRequiredRole = userRoles.includes(REQUIRED_ROLE_ID)
-
-    if (hasRequiredRole) {
-      await handleUpdateVerification(telegramChatId, hasRequiredRole)
-      const successMessage = `
-      ✅ **You are a verified member ${user.username}!** 🎉
-      
-      Welcome, validator! Your role has been verified, you now have access to exclusive announcements and updates.
-        `
-      await sendTelegramMessage(telegramChatId, successMessage, hasRequiredRole)
-      res.send('Success!')
-    } else {
-      const failureMessage = `❌ Hello ${user.username}, you do not have the required role.`
-      await sendTelegramMessage(telegramChatId, failureMessage, hasRequiredRole)
-      res.send('Failure!')
-    }
+    const member = response.data
+    return member.roles.includes(REQUIRED_ROLE_ID)
   } catch (error) {
-    console.error(`Error during Discord authentication or role checking:`, error.response?.data || error.message)
-    res.send('Authentication failed.')
+    logger.error(`Error checking user roles: ${error.message}`)
+    throw error
   }
-})
+}
 
-// Default route for invalid paths
-app.get('/', (req, res) => {
-  res.send('Invalid route. Please use the correct callback URL.')
-})
+async function refreshAccessToken() {
+  try {
+    const response = await axios.post(
+      DISCORD_API_OAUTH2_URL,
+      new URLSearchParams({
+        client_id: CLIENT_ID,
+        client_secret: CLIENT_SECRET,
+        grant_type: 'refresh_token',
+        refresh_token: refreshToken,
+        redirect_uri: DISCORD_REDIRECT_URI,
+        scope: 'identify',
+      }),
+      {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      },
+    )
 
-// Start the server
-app.listen(port, () => {
-  logger.info(`Discord auth server running on http://localhost:${port}`)
-})
+    const { access_token, refresh_token } = response.data
+
+    accessToken = access_token
+    refreshToken = refresh_token
+
+    logger.info(`Access token refreshed: ${accessToken}`)
+
+    return response.data
+  } catch (error) {
+    logger.error(`Error refreshing access token: ${error.response?.data || error.message}`)
+    throw error
+  }
+}
 
 async function handleUpdateVerification(chatId, isVerifedValidator) {
   try {
@@ -113,7 +187,7 @@ async function sendTelegramMessage(chatId, message, isVerifed) {
     inline_keyboard: [[{ text: 'Subscribe To Discord Announcements 📢', callback_data: 'discord_announcements' }]],
   }
 
-  const notVerifiedButtons = callbackButtonForDiscordNotVerify()
+  const notVerifiedButtons = callbackButtonForDiscordNotVerify(chatId)
 
   try {
     const url = `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`
